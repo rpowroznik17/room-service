6b3fc550b26e1bcbda6490de1be1a410895ed2c9
krzysztofszczybylo <s218037@student.uek.krakow.pl>
2023-05-02 18:47:05 +0200

    displayImage at offerdetails

--- src/views/OfferDetails.vue
+++ src/views/OfferDetails.vue
@@ -2,6 +2,7 @@
   <div>
     <router-link to="/offers">Go back to offers</router-link>
     <h1>{{ offer.title }}</h1>
+    <img :src="offer.imageURL" alt="Offer Image" v-if="offer.imageURL">
     <p>Price per night: {{ offer.price_per_night }} zł</p>
     <p>Max amount of people: {{ offer.max_amount_of_people }}</p>
     <p>{{ offer.description }}</p>
@@ -13,27 +14,27 @@
   </div>
 </template>
 <script>
-import { db } from "../main.js";
+import { db, storage } from "../main.js";
 import { getDoc, doc, addDoc, collection } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { useRouter } from "vue-router";
+import { ref, getDownloadURL } from "firebase/storage";
 export default {
-  data() {
-    return {
-      offer: null,
-      counter: 0,
-      router: useRouter()
-  };
-  },
-
+    data() {
+      return {
+        offer: null,
+        counter: 0,
+        router: useRouter()
+    };
+    },
   computed: {
     totalPrice() {
       if (this.offer) {
         return this.counter * this.offer.price_per_night;
-  }   else {
+    } else {
         return 0;
-  }
-  },
+    }
+    },
   },
 
   async created() {
@@ -42,6 +43,11 @@ export default {
     const docSnap = await getDoc(docRef);
     if (docSnap.exists()) {
       this.offer = docSnap.data();
+    if (this.offer.imageFileName) {
+      const imageRef = ref(storage, `images/${this.offer.imageFileName}`);
+      const imageURL = await getDownloadURL(imageRef);
+      this.offer.imageURL = imageURL;
+    }
     } else {
       console.log("Error");
     }
@@ -49,25 +55,25 @@ export default {
 
   methods: {
     async bookOffer() {
-      const user = getAuth().currentUser;
-      const offerId = this.$route.params.id;
-      const docRef = doc(db, "offers", offerId);
-      if (user) {
-        const booking = {
-        nights: this.counter,
-        total_price: this.totalPrice,
-        client: user.uid,
-        offer: docRef.id,
-      };
-      try {
-        const docRef = await addDoc(collection(db, "bookings"), booking);
-        console.log("Booking created with ID: ", docRef.id);
-        this.router.push("/offers");
+    const user = getAuth().currentUser;
+    const offerId = this.$route.params.id;
+    const docRef = doc(db, "offers", offerId);
+    if (user) {
+      const booking = {
+      nights: this.counter,
+      total_price: this.totalPrice,
+      client: user.uid,
+      offer: docRef.id,
+    };
+    try {
+      const docRef = await addDoc(collection(db, "bookings"), booking);
+      console.log("Booking created with ID: ", docRef.id);
+      this.router.push("/offers");
       } catch (e) {
-        console.error("Error adding booking: ", e);
-        }
-      }
+          console.error("Error adding booking: ", e);
+    }
+    }
     },
-  },
+    },
 };
 </script>
